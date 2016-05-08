d7077643d58807d5ecdb147cdd3ff15da7148170
Eric Yang <windfarer@gmail.com>
2016-05-08 22:16:54 +0800

    resizingcapacitystack

--- ch1/ResizingCapacityStack.java
+++ ch1/ResizingCapacityStack.java
@@ -1,19 +1,16 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import java.util.Iterator;
 
-public class FixedCapacityStack<Item> {
-    private Item[] a;
-    private int N;
+public class ResizingCapacityStack<Item> implements Iterable<Item> {
+    private Item[] a = (Item[]) new Object[1];
+    private int N = 0;
     private void resize(int max) {
         Item[] temp = (Item[]) new Object[max];  //create new array first
         for (int i=0; i<N; i++) {
             temp[i] = a[i];
         }
         a = temp;
-    }
-    
-    public FixedCapacityStack(int cap) {
-        a = (Item[]) new Object[cap];
     }
     public boolean isEmpty() {
         return N == 0;
@@ -31,18 +28,18 @@ public class FixedCapacityStack<Item> {
         if (N>0 && N == a.length/4) resize(a.length/2);
         return item;
     }
-    public static void main(String[] args) {
-        FixedCapacityStack<String> s;
-        s = new FixedCapacityStack<String>(4);
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) {
-                s.push(item);
-            }
-            else if (!s.isEmpty()) {
-                StdOut.print(s.pop() + " ");
-            }
+      public Iterator<Item> iterator(){ 
+        return new ReverseArrayIterator(); 
+    }
+      
+    public class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+        public boolean hasNext() {
+            return i > 0;
         }
-        StdOut.println("(" + s.size() + " left on stack)");
+        public Item next() {
+            return a[--i];
+        }
+        public void remove() {}
     }
 }
